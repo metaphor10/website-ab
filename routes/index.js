fd556c55c3110e8c7a0d3bb25994fb7ee6b8d344
aboroch <aboroch@us.ibm.com>
2017-04-25 19:00:38 -0400

    change

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var React = require('react'),
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', {title: 'Hello World, AU ACM', htmlReact: htmlReact});
+    res.render('index', {title: 'Hello World, nooooooooooooooooooo', htmlReact: htmlReact});
 });
 
 module.exports = router;
